2d747b4359f666abac5fb2b2ec77fedf3fd696a3
DxsSucuk <53257574+DxsSucuk@users.noreply.github.com>
2024-01-09 12:21:56 +0100

    Login und stuff.

--- Frontend/src/routes/+layout.svelte
+++ Frontend/src/routes/+layout.svelte
@@ -1,10 +1,63 @@
-<script lang="ts">
-	import '../app.postcss';
+<script lang='ts'>
+    import '../app.postcss';
 
-	// Floating UI for Popups
-	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
-	import { storePopup } from '@skeletonlabs/skeleton';
-	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+    import { AppShell } from '@skeletonlabs/skeleton';
+    import { Avatar } from '@skeletonlabs/skeleton';
+    import { AppBar } from '@skeletonlabs/skeleton';
+    import { localStorageStore } from '@skeletonlabs/skeleton'; // Backs up our data
+    import type { Writable } from 'svelte/store'; // Handles our data
+    import type { Settings } from '$lib/client';
+
+    // Floating UI for Popups
+    import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
+    import { storePopup } from '@skeletonlabs/skeleton';
+    import { onMount } from 'svelte';
+
+    storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+
+    const settingStore: Writable<Settings[]> = localStorageStore('settingsStore', []);
+
+    function saveSetting(setting: Settings) {
+        $settingStore = [
+            setting,
+            ...$settingStore
+        ];
+    }
+
+    function getSetting(name: string) : Settings {
+        return $settingStore.filter(setting => setting.name === name)[0];
+    }
+
+	function deleteSetting(name: string) {
+        $settingStore = $settingStore.filter(setting => setting.name !== name);
+    }
+
+
+    let profilePicture: string | undefined;
+
+    onMount(() => {
+        if (!getSetting("profile")) {
+            let setting = {
+                name: "profile",
+                value: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/0325da39-2b46-4334-909f-5be445287d5b/de6rxcp-fac48cdd-4a90-4abd-a1b6-82702ab85e0d.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzAzMjVkYTM5LTJiNDYtNDMzNC05MDlmLTViZTQ0NTI4N2Q1YlwvZGU2cnhjcC1mYWM0OGNkZC00YTkwLTRhYmQtYTFiNi04MjcwMmFiODVlMGQucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.09EyMJkdd0TZIFnJwIugt1_K3Cpbw3lf41fZtWkDVeA"
+            };
+
+            saveSetting(setting);
+        }
+        profilePicture = getSetting("profile").value;
+    });
 </script>
 
-<slot />
+<AppShell>
+    <AppBar gridColumns='grid-cols-3' slotDefault='place-self-center' slotTrail='place-content-end'>
+        <svelte:fragment slot='lead'>(icon)</svelte:fragment>
+        VerbesserteKlausurNachreibenTerminFinder
+        <svelte:fragment slot='trail'>
+            {#if profilePicture}
+                <Avatar src={profilePicture} width='w-16' rounded='rounded-full' />
+            {/if}
+        </svelte:fragment>
+    </AppBar>
+    <slot />
+    <svelte:fragment slot='footer'>Datum</svelte:fragment>
+</AppShell>
--- Frontend/src/routes/+page.svelte
+++ Frontend/src/routes/+page.svelte
@@ -1,41 +1,55 @@
 <script lang="ts">
-	import { AppShell } from '@skeletonlabs/skeleton';
-	import { Avatar } from '@skeletonlabs/skeleton';
-	import { AppBar } from '@skeletonlabs/skeleton';
-	import { Table } from '@skeletonlabs/skeleton';
-	import { tableMapperValues } from '@skeletonlabs/skeleton';
-	import type { TableSource } from '@skeletonlabs/skeleton';
-	
-const sourceData = [
-	{ position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-	{ position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-	{ position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-	{ position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-	{ position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-];
+	import { goto } from '$app/navigation';
+	import { login } from '$lib/client';
 
+	let username:string, password:string;
 
-const tableSimple: TableSource = {
-	// A list of heading labels.
-	head: ['Lernfeld', 'Datum', 'Anzahl'],
-	// The data visibly shown in your table body UI.
-	body: tableMapperValues(sourceData, ['name', 'symbol', 'weight']),
-	// Optional: The data returned when interactive is enabled and a row is clicked.
-	meta: tableMapperValues(sourceData, ['position', 'name', 'symbol', 'weight']),
-	// Optional: A list of footer labels.
-	foot: ['Total', '', '<code class="code">5</code>']
-};
-				
+	function loginForm() {
+		var result = login(username, password);
+
+		if (result) {
+			goto('dash');
+		} else {
+			console.error('Login failed!');
+		}
+	}
+
 </script>
 
-<AppShell>
-	<AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
-		<svelte:fragment slot="lead">(icon)	</svelte:fragment>
-		VerbesserteKlausurNachreibenTerminFinder
-		<svelte:fragment slot="trail"><Avatar src="https://images.unsplash.com/photo-1617296538902-887900d9b592?ixid=M3w0Njc5ODF8MHwxfGFsbHx8fHx8fHx8fDE2ODc5NzExMDB8&ixlib=rb-4.0.3&w=128&h=128&auto=format&fit=crop" width="w-16" rounded="rounded-full" />
-	</svelte:fragment>
-	</AppBar>
-		<Table source={tableSimple} />
-		<slot />
-	<svelte:fragment slot="footer">Datum</svelte:fragment>
-</AppShell>
+
+<section class="bg-gray-50 dark:bg-gray-900">
+  <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+      <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                  Login
+              </h1>
+              <form class="space-y-4 md:space-y-6" on:submit|preventDefault={loginForm}>
+                  <div>
+                      <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username</label>
+                      <input type="text" name="name" bind:value={username} id="name" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="max_musterman" required="">
+                  </div>
+                  <div>
+                      <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                      <input type="password" name="password" bind:value={password} id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
+                  </div>
+                  <div class="flex items-center justify-between">
+                      <div class="flex items-start">
+                          <div class="flex items-center h-5">
+                            <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required="">
+                          </div>
+                          <div class="ml-3 text-sm">
+                            <label for="remember" class="text-gray-500 dark:text-gray-300">Remember me</label>
+                          </div>
+                      </div>
+                      <a href="#" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
+                  </div>
+                  <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
+                  <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+                      Don’t have an account yet? <a href="#" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
+                  </p>
+              </form>
+          </div>
+      </div>
+  </div>
+</section>
