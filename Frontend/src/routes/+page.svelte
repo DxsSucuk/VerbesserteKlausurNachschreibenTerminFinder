541240335821ff873bb19a86ff37feab8c2d440f
Firedragon2104 <95493902+Firedragon2104@users.noreply.github.com>
2024-01-09 09:47:46 +0000

    Dashbord

--- Frontend/src/routes/+page.svelte
+++ Frontend/src/routes/+page.svelte
@@ -1,15 +1,41 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+<script lang="ts">
+	import { AppShell } from '@skeletonlabs/skeleton';
+	import { Avatar } from '@skeletonlabs/skeleton';
+	import { AppBar } from '@skeletonlabs/skeleton';
+	import { Table } from '@skeletonlabs/skeleton';
+	import { tableMapperValues } from '@skeletonlabs/skeleton';
+	import type { TableSource } from '@skeletonlabs/skeleton';
+	
+const sourceData = [
+	{ position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+	{ position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
+	{ position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
+	{ position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
+	{ position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
+];
 
-<div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5">
-		<h1 class="h1">Let's get cracking bones!</h1>
-		<p>Start by exploring:</p>
-		<ul>
-			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout</li>
-			<li><code class="code">/src/app.postcss</code> - app wide css</li>
-			<li>
-				<code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents
-			</li>
-		</ul>
-	</div>
-</div>
+
+const tableSimple: TableSource = {
+	// A list of heading labels.
+	head: ['Lernfeld', 'Datum', 'Anzahl'],
+	// The data visibly shown in your table body UI.
+	body: tableMapperValues(sourceData, ['name', 'symbol', 'weight']),
+	// Optional: The data returned when interactive is enabled and a row is clicked.
+	meta: tableMapperValues(sourceData, ['position', 'name', 'symbol', 'weight']),
+	// Optional: A list of footer labels.
+	foot: ['Total', '', '<code class="code">5</code>']
+};
+				
+</script>
+
+<AppShell>
+	<AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
+		<svelte:fragment slot="lead">(icon)	</svelte:fragment>
+		VerbesserteKlausurNachreibenTerminFinder
+		<svelte:fragment slot="trail"><Avatar src="https://images.unsplash.com/photo-1617296538902-887900d9b592?ixid=M3w0Njc5ODF8MHwxfGFsbHx8fHx8fHx8fDE2ODc5NzExMDB8&ixlib=rb-4.0.3&w=128&h=128&auto=format&fit=crop" width="w-16" rounded="rounded-full" />
+	</svelte:fragment>
+	</AppBar>
+		<Table source={tableSimple} />
+		<slot />
+	<svelte:fragment slot="footer">Datum</svelte:fragment>
+</AppShell>
