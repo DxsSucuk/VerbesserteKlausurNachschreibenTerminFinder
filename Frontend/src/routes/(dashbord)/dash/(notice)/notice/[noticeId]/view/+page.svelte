4fd1879a7ce96aa1c0b254d867f2c4e0f59c7489
DxsSucuk <53257574+DxsSucuk@users.noreply.github.com>
2024-03-20 12:19:01 +0100

    d

---
+++ Frontend/src/routes/(dashbord)/dash/(notice)/notice/[noticeId]/view/+page.svelte
@@ -0,0 +1,31 @@
+<script lang='ts'>
+    import Icon from '@iconify/svelte';
+    import { goto } from '$app/navigation';
+    import { checkSession, getNoticeImage, approveNotice } from '$lib/client';
+    import { onMount } from 'svelte';
+    import { page } from '$app/stores';
+
+    let noticeId = $page.params.noticeId;
+    let noticeImage: string;
+
+    onMount(async () => {
+        var result = await checkSession();
+        if (!result) {
+            goto('/');
+        } else {
+            noticeImage = await getNoticeImage(noticeId);
+        }
+    });
+</script>
+
+<!-- Make a div with a image and two buttons -->
+<!-- One to approve it and one to deny it -->
+<div>
+    <img alt="gaysex" src={noticeImage} width='1024' height='1024' />
+    <button on:click={() => approveNotice(noticeId)}>
+        <Icon icon='mdi-light:check-circle' width='24' height='24' />
+    </button>
+    <button>
+        <Icon icon='material-symbols-light:cancel-outline' width='24' height='24' />
+    </button>
+</div>
